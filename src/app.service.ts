f51afdcf57cd0c747b05f1d0be9d5da36d1f9f4d
Romario Marques Melo <romario.melos@gmail.com>
2021-11-11 11:08:05 -0300

    Fisrt commit

---
+++ src/app.service.ts
@@ -0,0 +1,88 @@
+import { Injectable } from '@nestjs/common';
+import * as puppeteer from 'puppeteer';
+
+interface IJsonArray {
+  artType: string;
+  urlTitle: string;
+  numberPage: string;
+  pubOrder: string;
+  hierarchyStr: string;
+  hierarchyList: string[];
+  title: string;
+  content: string;
+  hierarchyLevelSize: number;
+}
+
+@Injectable()
+export class AppService {
+  async getHello() {
+    // let links: string[] = [];
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    try {
+      const orgao = encodeURI('Ministério de Minas e Energia');
+      const subOrgao = encodeURI('Agência Nacional de Energia Elétrica');
+      const secao = encodeURI('do1');
+      const uri = `https://www.in.gov.br/leiturajornal?org=${orgao}&org_sub=${subOrgao}&secao=${secao}`;
+      // const uri = 'https://www.in.gov.br/leiturajornal';
+      await page.goto(uri);
+
+      const params = JSON.parse(
+        await page.$eval<string>('#params', (el) => el.innerHTML),
+      );
+
+      const jsonArray: IJsonArray[] = params.jsonArray;
+
+      let countMinasEEnergia = 0;
+      for (const item of jsonArray) {
+        if (
+          item.hierarchyList.some(
+            (org) => org === 'Ministério de Minas e Energia',
+          ) &&
+          item.hierarchyList.some(
+            (org) => org === 'Agência Nacional de Energia Elétrica',
+          )
+        ) {
+          console.log(item.title);
+          countMinasEEnergia++;
+          await page.waitForTimeout(10000);
+          await page.goto(
+            `https://www.in.gov.br/en/web/dou/-/${item.urlTitle}`,
+          );
+          const texts = await page.$$eval(
+            'div.texto-dou',
+            (el: HTMLDivElement[]) => {
+              return el.map((e) => e.innerText);
+            },
+          );
+          console.log(texts);
+        }
+      }
+      console.log(countMinasEEnergia);
+
+      // let button: puppeteer.ElementHandle<Element>;
+
+      // let countPage: number = 0;
+      // do {
+      //   links = [...links, ...(await this.getLinksByPage(page))];
+
+      //   button = await page.$(
+      //     'span.pagination-button.page-num.active + span.pagination-button.page-num'
+      //   );
+
+      //   if (button) {
+      //     await button.click();
+      //   }
+      //   countPage++;
+      // } while (button);
+
+      // console.log('Pages:', countPage);
+      // console.log(links);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      await page.close();
+      await browser.close();
+    }
+  }
+}
